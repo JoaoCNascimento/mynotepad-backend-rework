e4f878f0e2afc37f291ea71814f66c67d2a002ff
JoaoCNascimento <joao.nascimento0576@gmail.com>
2022-02-05 13:25:27 -0300

    Atualizando config

--- config/config.js
+++ config/config.js
@@ -3,11 +3,13 @@ require('dotenv').config();
 if (process.env.NODE_ENV == "production") {
     module.exports = {
         connectionString: process.env.DB_PRODUCTION_CONNECTION_STRING,
-        PORT: process.env.PORT
+        PORT: process.env.PORT,
+        loginUrl: "https://mynotepad-com.herokuapp.com/login"
     };
 } else {
     module.exports = {
         connectionString: process.env.DB_DEVELOPMENT_CONNECTION_STRING,
-        PORT: 8089
+        PORT: 8089,
+        loginUrl: "http://localhost:4200/login"
     };
 }
